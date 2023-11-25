82e6817e95a139e942d42bb2133609038809be5e
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-11-25 09:49:12 +0700

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Rifqi Muhammad Aziz on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
